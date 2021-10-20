7ca98fc84dc23a51eefa4514e1852030574f1aea
Pablo Leal <pabloleal7@hotmail.com>
2021-10-20 20:07:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+//document.querySelector('.message').textContent
+
+
+document.querySelector('.message').textContent = 'Numero Correto!'
+
+document.querySelector(".number").textContent = "13"
+
+document.querySelector('.score').textContent = "10"
+
+
+
+
+document.querySelector('.guess').value = 23
+
